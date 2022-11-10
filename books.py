bac5dddc1a63369c65a162fc6671f981b05cfd83
divyuk <div14kes@gmail.com>
2022-11-10 15:02:36 +0530

    Using Enumeration to show parameter in Swagger

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from enum import Enum
 app = FastAPI()
 BOOKS = {
     'book_1' : {'title' : 'Sapiens', 'author' : 'Yuval Noah Harari'}, 
@@ -9,6 +9,30 @@ BOOKS = {
     'book_5' : {'title' : 'Hyperfocus', 'author':'Chris Bailey'},
 }
 
+class DirectionName(str,Enum):
+    north = "North"
+    south = "South"
+    east  = "East"
+    west  = "West"
+
 @app.get("/")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_id}")
+async def read_book(book_id : int):
+    return  {"book_title" : book_id}
+
+@app.get("/books/mybook")
+async def read_favourite_book():
+    return {"book_title" : "My favourite book"}
+
+@app.get("/directons/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {"Direction"  : direction_name, "sub" : "Up"}
+    if direction_name == DirectionName.south:
+        return {"Direction"  : direction_name, "sub" : "Down"}
+    if direction_name == DirectionName.west:
+        return {"Direction"  : direction_name, "sub" : "left"}
+    return {"Direction"  : direction_name, "sub" : "right"}
