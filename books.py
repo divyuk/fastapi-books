25b3d9a21bf0036745eab85b77479183bcc0f017
divyuk <div14kes@gmail.com>
2022-11-10 14:41:39 +0530

    Adding Books

--- books.py
+++ books.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+BOOKS = {
+    'book_1' : {'title' : 'Sapiens', 'author' : 'Yuval Noah Harari'}, 
+    'book_2' : {'title' : 'Deep Work', 'author' : 'Cal Newport'}, 
+    'book_3' : {'title' : 'Why we Sleep', 'author':'Matthew Walker'}, 
+    'book_4' : {'title' : 'Mindset', 'author':'Carol'},
+    'book_5' : {'title' : 'Hyperfocus', 'author':'Chris Bailey'},
+}
 
 @app.get("/")
-async def first_api():
-    return { 
-            "message" : "Hello! World"
-            }
+async def read_all_books():
+    return BOOKS
